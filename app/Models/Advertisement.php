140434718100958cf769b5c8603ac6a66626645c
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-12-04 11:06:52 +0200

    add soft delete advertisements

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Advertisement extends Model implements HasMedia
 {
-    use InteractsWithMedia;
+    use InteractsWithMedia, SoftDeletes;
 
     protected $fillable = [
         'title',
@@ -22,7 +23,7 @@ class Advertisement extends Model implements HasMedia
         'latitude',
         'longitude',
         'email',
-        'category_id'
+        'category_id',
     ];
 
     protected $dates = ['end_date', 'created_at'];

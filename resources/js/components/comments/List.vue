40f982443e29a31d023dd92ec71bda18480b5917
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-12-14 12:48:58 +0200

    add btn export users

--- resources/js/components/comments/List.vue
+++ resources/js/components/comments/List.vue
@@ -1,5 +1,10 @@
 <template>
     <div v-if="$parent.$attrs.auth">
+        <div style="margin-bottom: 30px" v-if="$parent.$attrs.auth.role === 'admin' && comments.length">
+            <preloader v-if="loading"></preloader>
+            <button class="btn btn-outline-primary"  @click.prevent="exportComments" v-else>Експорт комментариев</button>
+        </div>
+
         <comments-form @store="newComment" style="margin-bottom: 50px"></comments-form>
         <scroll-loader :loader-method="getData" :loader-enable="loadMore" style="display: block">
             <comment v-if="comments.length" v-for="(comment, index) in comments" :comment="comment"
@@ -17,21 +22,35 @@
 <script>
   import CommentsForm from './Form';
   import Comment from './Item';
+  import Preloader from './../Preloader';
 
   export default {
     name: "List",
     components: {
       Comment,
-      CommentsForm
+      CommentsForm,
+      Preloader
     },
     data() {
       return {
         comments: [],
-        url: 'comments/',
-        loadMore: true
+        url: `comments/${this.$route.params.advertisement}`,
+        loadMore: true,
+        loading: false,
       }
     },
     methods: {
+      exportComments(){
+        this.loading = true;
+        axios.get(`/exports/comments/${this.$route.params.advertisement}`).then(
+          window.Echo.channel(`comments`).listen('ExportComments', (e) => {
+            let file_name = `export-comments-${this.$route.params.advertisement}.csv`;
+            saveAs(e.data, file_name);
+            this.loading = false;
+            window.Echo.leave('advertisements');
+          })
+        )
+      },
       getData() {
         if (this.url) {
           axios.get(this.url)
@@ -48,9 +67,6 @@
       newComment(item) {
         this.comments.unshift(item);
       }
-    },
-    created() {
-      this.getData();
     }
   }
 </script>

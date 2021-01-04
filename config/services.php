b61e362554ec6fcbde3ff08a172df2fa9693b5e8
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-01-04 13:47:26 +0200

    add weather service

--- config/services.php
+++ config/services.php
@@ -29,5 +29,11 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'weather' => [
+        'key' => env('OPEN_WEATHER_API_KEY'),
+        'format_date' => 'd/m/Y',
+        'format_time' => 'h:i A',
+        'format_day' => 'l'
+    ]
 
 ];
